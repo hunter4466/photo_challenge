075031f6e2ae29dec59ff5de6eeb2caf851b07f8
Mario Chois <mario.chois.navarro@gmail.com>
2022-01-26 21:05:03 -0500

    RecycleView: Add dynamic views to display photos with different views

--- app/src/main/java/com/ravnnerdery/photo_challenge/photoList/PhotoListFragment.kt
+++ app/src/main/java/com/ravnnerdery/photo_challenge/photoList/PhotoListFragment.kt
@@ -42,4 +42,5 @@ class PhotoListFragment : Fragment() {
 
         return binding
     }
+
 }
--- app/src/main/java/com/ravnnerdery/photo_challenge/photoList/PhotosAdapter.kt
+++ app/src/main/java/com/ravnnerdery/photo_challenge/photoList/PhotosAdapter.kt
@@ -2,27 +2,65 @@ package com.ravnnerdery.photo_challenge.photoList
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ravnnerdery.photo_challenge.database.tables.Photo
+import com.ravnnerdery.photo_challenge.databinding.PhotoViewReverseBinding
 import com.ravnnerdery.photo_challenge.databinding.PhotoViewStraightBinding
 import com.squareup.picasso.Picasso
 
-class PhotosAdapter(private val clickListener: PhotoClickListener): ListAdapter<Photo, PhotosAdapter.ViewHolder>(PostListDiffCallBack()) {
+class PhotosAdapter(private val clickListener: PhotoClickListener) :
+    ListAdapter<Photo, PhotosAdapter.ViewHolder>(PostListDiffCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
+        return if (viewType == 1){
+            SubViewHolderStraight.from(parent)
+        } else {
+            SubViewHolderReverse.from(parent)
+        }
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item, clickListener)
+        holder.bind(getItem(position), clickListener)
     }
 
-    class ViewHolder private constructor (private val binding: PhotoViewStraightBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(item: Photo, clickListener: PhotoClickListener){
-            binding
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+        return if(item.id.toInt() % 2 == 0){
+            1
+        } else {
+            0
+        }
+    }
+
+    abstract class ViewHolder(val binding: ViewDataBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+         abstract fun bind(item: Photo, clickListener: PhotoClickListener)
+    }
+    class SubViewHolderStraight private constructor(binding: PhotoViewStraightBinding) :
+            ViewHolder(binding){
+                override fun bind(item: Photo, clickListener: PhotoClickListener){
+                    val binding = binding as PhotoViewStraightBinding
+                    binding.photo = item
+                    Picasso.get().load(item.thumbnailUrl).into(binding.thumbNailfromList)
+                    binding.clickListener = clickListener
+                    binding.executePendingBindings()
+                }
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = PhotoViewStraightBinding.inflate(layoutInflater, parent, false)
+                return SubViewHolderStraight(binding)
+            }
+        }
+            }
+    class SubViewHolderReverse private constructor(binding: PhotoViewReverseBinding) :
+        ViewHolder(binding){
+        override fun bind(item: Photo, clickListener: PhotoClickListener){
+            val binding = binding as PhotoViewReverseBinding
             binding.photo = item
             Picasso.get().load(item.thumbnailUrl).into(binding.thumbNailfromList)
             binding.clickListener = clickListener
@@ -31,13 +69,15 @@ class PhotosAdapter(private val clickListener: PhotoClickListener): ListAdapter<
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                return ViewHolder(PhotoViewStraightBinding.inflate(layoutInflater, parent, false))
+                val binding = PhotoViewReverseBinding.inflate(layoutInflater, parent, false)
+                return SubViewHolderReverse(binding)
             }
         }
     }
+
 }
 
-class PostListDiffCallBack : DiffUtil.ItemCallback<Photo>(){
+class PostListDiffCallBack : DiffUtil.ItemCallback<Photo>() {
     override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
         return oldItem.id == newItem.id
     }
@@ -47,6 +87,6 @@ class PostListDiffCallBack : DiffUtil.ItemCallback<Photo>(){
     }
 }
 
-class PhotoClickListener(val clickListener: (postId: Long) -> Unit){
-    fun onClick(post: Photo) = clickListener(post.id)
+class PhotoClickListener(val clickListener: (postId: Long) -> Unit) {
+    fun onClick(photo: Photo) = clickListener(photo.id)
 }
